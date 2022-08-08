1fd01508c1e69da636b1ef01787c016a633c46c7
AzureAD\JuanFernandoGomez <juanfgomeza@gmail.com>
2022-08-08 17:16:19 -0500

    Build tower delay

--- Assets/Tower/Tower.cs
+++ Assets/Tower/Tower.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float towerDelay = 3f;
+
+    void Start()
+    {
+        StartCoroutine(Build());
+    }
+
     public bool CreateTower(Tower tower, Vector3 position)
     {
         Bank bank = FindObjectOfType<Bank>();
@@ -15,10 +22,32 @@ public class Tower : MonoBehaviour
 
         if(bank.CurrentBalance >= cost)
         {
-            Instantiate(tower.gameObject, position, Quaternion.identity);
+            Instantiate(tower.gameObject, position, Quaternion.identity);            
             bank.Withdraw(cost);
             return true;
         }
         return false;        
+    }
+    IEnumerator Build()
+    {
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+        foreach(Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(towerDelay);
+            foreach(Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
+        
+        
     }
 }
